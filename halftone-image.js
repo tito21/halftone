1e0ab50d938ada27133d50876c1fd0a8158b0a1a
Alberto Di Biase <asdibiase@uc.cl>
2023-07-22 17:00:04 -0400

    Add alpha and draw the whole image

--- halftone-image.js
+++ halftone-image.js
@@ -1,11 +1,12 @@
 
 class HalftoneImage {
-    constructor(width, height, num_w, num_h, dots_scale, ca=105, ma=75, ya=90, ba=15) {
+    constructor(width, height, num_w, num_h, dots_scale, ca=105, ma=75, ya=90, ba=15, alpha) {
         this.width = width;
         this.height = height;
         this.num_w = num_w;
         this.num_h = num_h;
         this.dots_scale = dots_scale;
+        this.alpha = alpha;
 
         this.cyan = Array(num_w*num_h);
         this.cyan_angle = ca;
@@ -26,23 +27,25 @@ class HalftoneImage {
         fill(color);
         noStroke();
         // translate(angle, angle);
-        translate(width/2, height/2);
-        rotate(angle);
-        translate(-width/2, -height/2);
+        // translate(width/2, height/2);
+        // rotate(angle);
+        // translate(-width/2, -height/2);
         for (let i=0; i <= this.num_w; i++) {
             for (let j=0; j <= this.num_h; j++) {
                 let dot_size = arr[i*this.num_w+j];
-                ellipse(i*this.width/this.num_w, j*this.height/this.num_h, this.dots_scale*dot_size, this.dots_scale*dot_size);
+                let dx = this.dots_scale*cos(angle);
+                let dy = this.dots_scale*sin(angle);
+                ellipse(i*this.width/this.num_w + dx, j*this.height/this.num_h + dy, this.dots_scale*dot_size, this.dots_scale*dot_size);
             }
         }
         pop();
     }
 
     render() {
-        this._render(color(0, 255, 255, 255), this.cyan, this.cyan_angle);
-        this._render(color(255, 0, 255, 255), this.magenta, this.magenta_angle);
-        this._render(color(255, 255, 0, 255), this.yellow, this.yellow_angle);
-        this._render(color(0, 0, 0, 255), this.black, this.black_angle);
+        this._render(color(0, 255, 255, this.alpha), this.cyan, this.cyan_angle);
+        this._render(color(255, 0, 255, this.alpha), this.magenta, this.magenta_angle);
+        this._render(color(255, 255, 0, this.alpha), this.yellow, this.yellow_angle);
+        this._render(color(0, 0, 0, this.alpha), this.black, this.black_angle);
     }
 
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 
 let width = 1280;
 let height = 857;
+let max_size = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
 
 let halftone_img;
 let img;
@@ -12,82 +13,37 @@ let cyan_angle;
 let magenta_angle;
 let yellow_angle;
 let black_angle;
+let alpha;
 let img_loader;
 
 let img_canvas;
 
 function build_arrays() {
 
+  img_canvas.width = width;
+  img_canvas.height = height;
+
   halftone_img = new HalftoneImage(width, height,
                                    num_x.value(), num_y.value(),
                                    dots_size.value(),
-                                   cyan_angle.value(), magenta_angle.value(), yellow_angle.value(), black_angle.value());
+                                   cyan_angle.value(), magenta_angle.value(), yellow_angle.value(), black_angle.value(),
+                                   alpha.value());
   // img.loadPixels();
-
-  // Black
-  img_canvas.push();
-  img_canvas.translate(width/2, height/2);
-  img_canvas.rotate(-halftone_img.black_angle);
-  img_canvas.translate(-width/2, -height/2);
   img_canvas.image(img, 0, 0);
   for (let i=0; i <= halftone_img.num_w; i++) {
     for (let j=0; j <= halftone_img.num_h; j++) {
       let c = img_canvas.get(i*width/halftone_img.num_w, j*height/halftone_img.num_h);
+
+      // let c = img_canvas.get(i*(width + width/2)/halftone_img.num_w, j*(height + height/2)/halftone_img.num_h);
       // print(c);
       let K = 1-Math.max(c[0]/255, c[1]/255, c[2]/255)
       halftone_img.black[i*halftone_img.num_w+j] = K;
-    }
-  }
-  img_canvas.pop();
-
-  // Cyan
-  img_canvas.push();
-  img_canvas.translate(width/2, height/2);
-  img_canvas.rotate(-halftone_img.cyan_angle);
-  img_canvas.translate(-width/2, -height/2);
-  img_canvas.image(img, 0, 0);
-  for (let i=0; i <= halftone_img.num_w; i++) {
-    for (let j=0; j <= halftone_img.num_h; j++) {
-      let c = img_canvas.get(i*width/halftone_img.num_w, j*height/halftone_img.num_h);
-      // print(c);
-      let K = 1-Math.max(c[0]/255, c[1]/255, c[2]/255)
       halftone_img.cyan[i*halftone_img.num_w+j] = (1 - c[0]/255 - K) / (1 - K) ;
-    }
-  }
-  img_canvas.pop();
-
-  // Magenta
-  img_canvas.push();
-  img_canvas.translate(width/2, height/2);
-  img_canvas.rotate(-halftone_img.magenta_angle);
-  img_canvas.translate(-width/2, -height/2);
-  img_canvas.image(img, 0, 0);
-  for (let i=0; i <= halftone_img.num_w; i++) {
-    for (let j=0; j <= halftone_img.num_h; j++) {
-      let c = img_canvas.get(i*width/halftone_img.num_w, j*height/halftone_img.num_h);
-      // print(c);
-      let K = 1-Math.max(c[0]/255, c[1]/255, c[2]/255)
       halftone_img.magenta[i*halftone_img.num_w+j] = (1 - c[1]/255 - K) / (1 - K) ;
-    }
-  }
-  img_canvas.pop();
-
-  // Yellow
-  img_canvas.push();
-  img_canvas.translate(width/2, height/2);
-  img_canvas.rotate(-halftone_img.yellow_angle);
-  img_canvas.translate(-width/2, -height/2);
-  img_canvas.image(img, 0, 0);
-  for (let i=0; i <= halftone_img.num_w; i++) {
-    for (let j=0; j <= halftone_img.num_h; j++) {
-      let c = img_canvas.get(i*width/halftone_img.num_w, j*height/halftone_img.num_h);
-      // print(c);
-      let K = 1-Math.max(c[0]/255, c[1]/255, c[2]/255)
       halftone_img.yellow[i*halftone_img.num_w+j] = (1 - c[2]/255 - K) / (1 - K) ;
+
     }
   }
-  img_canvas.pop();
-
 
   draw();
 
@@ -102,6 +58,8 @@ function setup() {
   // createCanvas(width, height, SVG);
   createCanvas(width, height);
   img_canvas = createGraphics(width, height);
+  angleMode(DEGREES);
+  ellipseMode(CENTER);
 
   num_x = select('#num_x');
   num_x.mouseReleased(build_arrays);
@@ -117,6 +75,8 @@ function setup() {
   yellow_angle.mouseReleased(build_arrays);
   black_angle = select('#black_angle');
   black_angle.mouseReleased(build_arrays);
+  alpha = select('#alpha');
+  alpha.mouseReleased(build_arrays);
 
   save_botton = select('#save');
   save_botton.mouseClicked(() => save('frame.jpg'));
@@ -150,6 +110,7 @@ function load_new_img(event) {
       console.log("loaded");
       width = img.width;
       height = img.height;
+      max_size = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
       resizeCanvas(width, height);
       setup();
     });
